eccb46d6f613f6488bd41c5ec78df3e968b9baa8
Akmal Rasulov <89010611+akirasulov@users.noreply.github.com>
2024-10-18 16:54:48 +0500

    style: fix code style

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,6 @@
 <?php
 
-declare (strict_types = 1);
+declare(strict_types=1);
 
 use App\Factories\ErrorFactory;
 use Illuminate\Foundation\Application;
@@ -8,7 +8,6 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use JustSteveKing\Tools\Http\Enums\Status;
 use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -29,10 +28,10 @@ return Application::configure(basePath: dirname(__DIR__))
             'verified' => App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
     })
-    ->withExceptions(function (Exceptions $exceptions) {
+    ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(fn(UnprocessableEntityHttpException $exception, Request $request) => new JsonResponse(
             data: $exception->getMessage(),
-            status: 422
+            status: 422,
         ));
 
         $exceptions->render(fn(Throwable $exception, Request $request) => ErrorFactory::create(
